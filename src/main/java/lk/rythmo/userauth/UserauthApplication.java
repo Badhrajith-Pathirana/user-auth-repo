4e3767ca87e36658ac2149138a28e12967f291b1
badhrajith-pathirana <badhrajith.pathirana@gmail.com>
2021-01-03 11:57:09 +0530

    Bugfixes

--- src/main/java/lk/rythmo/userauth/UserauthApplication.java
+++ src/main/java/lk/rythmo/userauth/UserauthApplication.java
@@ -1,12 +1,22 @@
 package lk.rythmo.userauth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableDiscoveryClient
 @EnableTransactionManagement
 public class UserauthApplication {
 
@@ -14,4 +24,17 @@ public class UserauthApplication {
 		SpringApplication.run(UserauthApplication.class, args);
 	}
 
+}
+
+@RestController
+class ServiceInstanceRestController {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(
+			@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
+	}
 }
